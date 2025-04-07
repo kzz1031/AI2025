e0f02bc91346e6164ecda4eaf87dd41ddf3d74c9
kzz1031 <22300240022@m.fudan.edu.cn>
2025-04-07 02:40:04 +0000

    adjust the structure

--- Week4-CNN/lab2_mnist.py
+++ Week4-CNN/lab2_mnist.py
@@ -100,7 +100,7 @@ if torch.cuda.is_available():
 else:
     print("cpu")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = MNISTNet().to(device)
+model = MNISTNet(kernel_size=10).to(device)
 optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=0.0001)
 # 添加学习率调度器
 scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 20], gamma=0.1)
